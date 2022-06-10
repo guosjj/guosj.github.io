9cd129da1d3ed828800d7bc044cd7a1d13824e45
“guosj” <2547361543@qq.com>
2022-06-10 23:46:30 +0800

    修改端口

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
 const request = axios.create({
-	baseURL: 'http://localhost:8090/',
+	baseURL: 'http://121.196.111.86:8090/',
 	timeout: 10000,
 })
 
